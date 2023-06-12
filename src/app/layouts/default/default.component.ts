907aac578e4ab1d0b4b8743996d0acf3114c1299
Sableprashant96 <sableprashant96@gmail.com>
2023-06-12 19:45:45 +0530

    initial commit

---
+++ src/app/layouts/default/default.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.scss']
+})
+export class DefaultComponent {
+
+}
